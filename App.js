68172c9bdc645cda6786bcd7e35a7388c8c7edc5
pashyanthi <67546604+pashyanthi@users.noreply.github.com>
2021-05-26 17:09:25 +0530

    project c68

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import{createBottomTabNavigator}from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import InstagramScreen from './screens/inst'
+import FacebookScreen from './screens/fb';
+
+// or any pure javascript modules available in npm
+//import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <AppContainer/>
+      
+       </View>
+    )
+  }
 }
+
+const TabNavigator=createBottomTabNavigator({
+  Instagram: {screen:InstagramScreen},
+  Facebook: {screen:FacebookScreen}
+})
+const AppContainer=createAppContainer(TabNavigator)
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rosybrown',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 20
   },
 });
---
+++ screens/inst.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default class InstagramScreen extends React.Component{
+    render(){
+        return(
+     <view style ={{flex:1,
+    justifyContent:"center",
+    alignItems:'center'
+    }}>
+        <Text>Instagram</Text>
+    </view>
+        )
+    }
+}
